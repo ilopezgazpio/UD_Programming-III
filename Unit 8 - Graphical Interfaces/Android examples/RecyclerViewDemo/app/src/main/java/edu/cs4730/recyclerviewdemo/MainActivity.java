80fcb47587b73b1b5784516ae7018e18cecb956a
Inigo Lopez-Gazpio <inigo.lopezgazpio@deusto.es>
2020-11-13 23:15:19 +0100

    add recyclerview example

---
+++ Unit 8 - Graphical Interfaces/Android examples/RecyclerViewDemo/app/src/main/java/edu/cs4730/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,50 @@
+package edu.cs4730.recyclerviewdemo;
+
+import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+/*
+ * A very simple example of how to setup a RecyclerView with cards views.
+ * A  RecyclerView.Adapter needs to be implemented inorder for it to work.
+ * There is not a "simple" adapter, it must be extended.  see myAdapter.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+    myAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<String> values = Arrays.asList(
+                "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2"
+        );
+
+        //setup the RecyclerView
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //and default animator
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        //setup the adapter, which is myAdapter, see the code.
+        mAdapter = new myAdapter(values, R.layout.my_row, this);
+        //add the adapter to the recyclerview
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
---
+++ Unit 8 - Graphical Interfaces/Android examples/RecyclerViewDemo/app/src/main/java/edu/cs4730/recyclerviewdemo/myAdapter.java
@@ -0,0 +1,76 @@
+package edu.cs4730.recyclerviewdemo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * this adapter is very similar to the adapters used for listview, except a ViewHolder is required
+ * see http://developer.android.com/training/improving-layouts/smooth-scrolling.html
+ * except instead having to implement a ViewHolder, it is implemented within the adapter.
+ */
+
+public class myAdapter extends RecyclerView.Adapter<myAdapter.ViewHolder> {
+
+    private List<String> myList;
+    private int rowLayout;
+    private Context mContext;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView myName;
+        public ImageView Pic;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            myName = (TextView) itemView.findViewById(R.id.Name);
+            Pic = (ImageView) itemView.findViewById(R.id.picture);
+        }
+    }
+
+    //constructor
+    public myAdapter(List<String> myList, int rowLayout, Context context) {
+        this.myList = myList;
+        this.rowLayout = rowLayout;
+        this.mContext = context;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(rowLayout, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        String entry = myList.get(i);
+        viewHolder.myName.setText(entry);
+        viewHolder.myName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv = (TextView) v;
+                Toast.makeText(mContext, tv.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewHolder.Pic.setImageResource(R.drawable.phone);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return myList == null ? 0 : myList.size();
+    }
+}
